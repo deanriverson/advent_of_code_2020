e635b8fa10a21130d3b66635caacbe1dfea8319f
Dean <diverson@vtti.vt.edu>
2020-12-12 10:55:08 -0700

    Solution for 3a

--- puzzle_3a.dart
+++ puzzle_3a.dart
@@ -1,38 +1,32 @@
 import 'file_utils.dart';
 
-class PwSpec {
-  final int min;
-  final int max;
-  final String char;
-  final String pw;
-
-  PwSpec(this.min, this.max, this.char, this.pw);
-
-  @override
-  String toString() => "PwSpec($min, $max, $char, $pw)";
-}
-
 void main() async {
-  final answer = await readLines('puzzle_2_input.txt')
-    .map(parseLine)
-    .map(checkSpec)
-    .fold(0, add);
-
-  print("answer is $answer");
+  final lines = await readLines('puzzle_3_input.txt').toList();
+  final answer = traverseMap(lines, 3, 1);
+  print('answer is $answer');
 }
 
-RegExp _regExp = new RegExp(r"(\d+)-(\d+) ([a-zA-Z]): (\w+)");
+int traverseMap(List<String> terrainList, int right, int down) {
+  assert(terrainList.length > 1);
 
-PwSpec parseLine(String line) {
-  RegExpMatch match = _regExp.firstMatch(line);
-  final xs = match.groups([1, 2, 3, 4]);
-  return PwSpec(int.parse(xs[0]), int.parse(xs[1]), xs[2], xs[3]);
+  final lineLen = terrainList[0].length;
+  final span = lineLen * down + right;
+  final terrain = terrainList.join();
+
+  int loc = 0;
+  int trees = 0;
+  int lineIdx = 0;
+
+  while (loc < terrain.length) {
+    trees += isTreeAtLoc(terrain, loc) ? 1 : 0;
+    
+    final nextLineIdx = (lineIdx + right) % lineLen;
+    loc += didOverflow(lineIdx, nextLineIdx) ? right : span;
+    lineIdx = nextLineIdx;
+  }
+  return trees;
 }
 
-int checkSpec(PwSpec spec) => (spec.pw.split(spec.char).length - 1).isBetween(spec.min, spec.max) ? 1 : 0;
+bool isTreeAtLoc(String terrain, int loc) => terrain[loc] == '#';
 
-extension on int {
-  bool isBetween(int min, int max) => this >= min && this <= max;
-}
-
-int add(sum, n) => sum += n;
+bool didOverflow(int last, int next) => next < last;
