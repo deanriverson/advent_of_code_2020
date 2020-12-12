7462973944ca8bff84d4066d22336cf5cedaa64a
Dean <diverson@vtti.vt.edu>
2020-12-12 09:46:30 -0700

    Fixed warnings in puzzle 2b

--- puzzle_3a.dart
+++ puzzle_3a.dart
@@ -29,15 +29,10 @@ PwSpec parseLine(String line) {
   return PwSpec(int.parse(xs[0]), int.parse(xs[1]), xs[2], xs[3]);
 }
 
-int checkSpec(PwSpec spec) {
-  try {
-  final char1 = spec.pw[spec.min-1] == spec.char;
-  final char2 = spec.pw[spec.max-1] == spec.char;
-  return char1 ^ char2 ? 1 : 0;
-  } catch (e) {
-    print("Error ${e.toString()} for spec: $spec");
-    return 0;
-  }
+int checkSpec(PwSpec spec) => (spec.pw.split(spec.char).length - 1).isBetween(spec.min, spec.max) ? 1 : 0;
+
+extension on int {
+  bool isBetween(int min, int max) => this >= min && this <= max;
 }
 
 int add(sum, n) => sum += n;
