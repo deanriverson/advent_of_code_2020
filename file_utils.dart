1ffd8abb2b0dfabf23e0f79e044f0e57fba635bc
Dean <diverson@vtti.vt.edu>
2020-12-12 20:52:58 -0700

    Solution to puzzle 4b

--- file_utils.dart
+++ file_utils.dart
@@ -6,3 +6,11 @@ Stream<String> readLines(String filePath) =>
     .openRead()
     .transform(utf8.decoder)
     .transform(LineSplitter());
+
+int parseAsInt(String val) => val == null ? -1 : int.tryParse(val) ?? -1;
+
+bool Function(int) isBetween(int min, int max) => (int val) => min <= val && val <= max;
+
+Function chain2<X, Y, R>(Y Function(X) fn1, R Function(Y) fn2) => (X val) => fn2(fn1(val));
+
+bool Function(String) createRegExpValidator(RegExp regExp) => (String val) => val == null ? false : regExp.hasMatch(val);
